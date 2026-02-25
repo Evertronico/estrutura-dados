f7cd0e4bc41ce33621706fc0a68c2f53ab911a4b
Daniel Queiroz Soares <danielsfelippe@gmail.com>
2026-02-25 20:55:40 -0300

    feat: Adicionando atividade1

---
+++ Atividade-praticas/Aula1/atividade1.py
@@ -0,0 +1,19 @@
+def somatorioDeNotas(qtdNotas):
+    for n in range(qtdNotas):
+        nota = int(input('Digite a sua nota: '))
+        if(nota > 0 and nota != null):
+            somaNotas += nota
+    return somaNotas
+
+def calculaMediaNota(qtdNotas: int):
+    somatorioDeNotas(qtdNotas)
+    return somaNotas/qtdNotas
+
+
+    
+def criaAluno():
+    return []
+
+
+def listaAlunos():
+    return []
